345314f9267ec80cd89975e0dd0b1bc6bdd8ba9e
qianlongxu <qianlongxu@sohu.com>
2016-12-30 16:09:40 +0800

    support iOS 8; 增加点播，直播 tab;

---
+++ LLLVideoPlayer/LLLTabbarController.swift
@@ -0,0 +1,14 @@
+//
+//  LLLTabbarViewController.swift
+//  LLLVideoPlayer
+//
+//  Created by 许乾隆 on 2016/12/30.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LLLTabbarController: UITabBarController {
+    
+}
