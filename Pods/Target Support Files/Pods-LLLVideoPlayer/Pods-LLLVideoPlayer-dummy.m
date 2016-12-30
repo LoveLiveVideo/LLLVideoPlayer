c40ef93218fbe2e8bec1943d4916bf4338df5ba6
qianlongxu <qianlongxu@sohu.com>
2016-12-30 18:23:14 +0800

    use pod；install Alamofire.

---
+++ Pods/Target Support Files/Pods-LLLVideoPlayer/Pods-LLLVideoPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LLLVideoPlayer : NSObject
+@end
+@implementation PodsDummy_Pods_LLLVideoPlayer
+@end
---
+++ Pods/Target Support Files/Pods-LLLVideoPlayer/Pods-LLLVideoPlayer-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LLLVideoPlayerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LLLVideoPlayerVersionString[];
+
