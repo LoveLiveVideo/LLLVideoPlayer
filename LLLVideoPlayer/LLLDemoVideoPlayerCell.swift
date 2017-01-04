524446086a105adede8e1c95f55dcd0d76bdced9
张科 <zhangke19880503@163.com>
2017-01-04 18:18:22 +0800

    demo架构

---
+++ LLLVideoPlayer/LLLDemoVideoPlayerCell.swift
@@ -0,0 +1,28 @@
+//
+//  LLLDemoVideoPlayerCell.swift
+//  LLLVideoPlayer
+//
+//  Created by ZK on 2017/1/4.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+import Foundation
+
+class LLLDemoVideoPlayerCell: UITableViewCell {
+    var imagePhoto: UIImageView?
+    var imagePhotoTitle: UILabel?
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        imagePhoto = UIImageView.init(frame: CGRect.init(x: 20, y: 6, width: 80, height: 40))
+        self.addSubview(imagePhoto!)
+        
+        imagePhotoTitle = UILabel.init(frame: CGRect.init(x: 120, y: 6, width: 200, height: 40))
+        self.addSubview(imagePhotoTitle!)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ LLLVideoPlayer/LLLDemoVideoPlayerController.swift
@@ -0,0 +1,53 @@
+//
+//  LLLVideoPlayerController.swift
+//  LLLVideoPlayer
+//
+//  Created by ZK on 2017/1/4.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+import Foundation
+
+class LLLDemoVideoPlayerController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //sohu的m3u8地址都加密了，防盗链，所以给ijk播放之前还的解密后能用的ts给
+    override func viewDidLoad() {
+        let xx: IJKFFMoviePlayerController = IJKFFMoviePlayerController.init()
+        let imageView: UIImageView = UIImageView.init()
+        imageView.sd_setImage(with: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: TodayTableViewCell? = tableView.dequeueReusableCell(withIdentifier: "TodayTableViewCell") as? TodayTableViewCell
+        if(cell == nil){
+            cell = TodayTableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: "TodayTableViewCell")
+        }
+        
+        let dic1: NSDictionary? = extensionsArray?.object(at: indexPath.row) as? NSDictionary
+        let data: NSData? = dic1?.object(forKey: "data") as? NSData
+        cell?.imagePhoto?.image = UIImage.init(data: data as! Data)
+        cell?.imagePhotoTitle?.text = dic1?.object(forKey: "title") as? String
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+            let storyboard: UIStoryboard = UIStoryboard.init(name: "MainInterface", bundle: Bundle.main)
+            let shareSelect: UIViewController = storyboard.instantiateViewController(withIdentifier: "ShareSelect")
+            let shareSelectVC: ShareSelectViewController = shareSelect as! ShareSelectViewController
+            let nav: UINavigationController = self.navigationController!
+            nav.pushViewController(shareSelectVC, animated: true)
+        }else if indexPath.row == 1 {
+            
+        }else {
+            
+        }
+    }
+}
